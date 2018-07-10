b81ea4ec7b9ad971e7d6b0e09ddf930640b46484
nicolascabrera909 <nicolascabrera909@hotmail.com>
2018-07-10 20:36:55 -0300

    con errores
    a ver si ves donde me equivoque

--- public/js/ingreso.js
+++ public/js/ingreso.js
@@ -1,61 +1,7 @@
+var app = angular.module('Ingreso', []);
 
-angular.module(Ingreso,[]).controller("IngresoControl", function($scope, $http){
+app.controller('IngresoControl', function($scope, $http) {
 
-//var app = angular.module('Ingreso',[]);
-//var app = angular.module('test', []);
-/* http://embed.plnkr.co/AI4qn8/
-	// this represents the state of the dialog: a visible flag and the person being edited
-	var EditPersonDialogModel = function () {
-		  this.visible = false;
-		};
-		EditPersonDialogModel.prototype.open = function(person) {
-		  this.person = person;
-		  this.visible = true;
-		};
-		EditPersonDialogModel.prototype.close = function() {
-		  this.visible = false;
-		};
-
-		app.controller('ctrl', ['$scope', function ($scope) {
-		  $scope.editDialog = new EditPersonDialogModel();
-		  
-		  $scope.persons = [{name: 'John'}, {name: 'Barbara'}];
-		  
-		  $scope.add = function() {
-		    $scope.persons.push({name: 'New Person'});
-		  };
-		}]);
-
-		app.directive('editPersonDialog', [function() {
-		  return {
-		    restrict: 'E',
-		    scope: {
-		      model: '=',
-		    },
-		    link: function(scope, element, attributes) {
-		      scope.$watch('model.visible', function(newValue) {
-		        var modalElement = element.find('.modal');
-		        modalElement.modal(newValue ? 'show' : 'hide');
-		      });
-		      
-		      element.on('shown.bs.modal', function() {
-		        scope.$apply(function() {
-		          scope.model.visible = true;
-		        });
-		      });
-
-		      element.on('hidden.bs.modal', function() {
-		        scope.$apply(function() {
-		          scope.model.visible = false;
-		        });
-		      });
-		      
-		    },
-		    templateUrl: 'edit-person-dialog.html',
-		  };
-		}]);    	
-*/
-//app.controller('IngresoControl',  function($scope, $http) {
     //Busco la persona	
 		$scope.submit = function() {
 		$scope.list = 'http://localhost:3000/api/personas/';
@@ -120,7 +66,7 @@ angular.module(Ingreso,[]).controller("IngresoControl", function($scope, $http){
 				console.log('No tiene afiliacion');
 				$scope.pasa = 'No tiene afiliacion';
 			}
-			EditPersonDialogModel();
+			
 	   	});
 	   	laPersona.error(function(data){
 	     	console.log('Error: ' + data);
