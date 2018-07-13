81ac3524b5f80fd28ba6cb135c15fc7b9471facc
aagussn <aguneb@hotmail.com>
2018-07-12 21:53:56 -0300

    Solucionado el tema de esperar afiliacion
    Solucionado el tema de esperar afiliacion

--- public/js/personas.js
+++ public/js/personas.js
@@ -1,108 +1,99 @@
-angular.module('personas', ['ngCookies'])
+var app = angular.module('personas', ['ngCookies'])
 
+app.controller('myController', function($scope, $http, $cookies, $q) {
 
-    .factory('getAfiliacion', function(){
-        return {
-            getAfi: function(id) {
-                return $http.get('http://localhost:3000/api/afiliacions/' + id );
-            }
-        }
-    })
-    .controller('myController', function($scope, $http, $cookies) {
+    $scope.data = [];
+    
+    var request = $http.get('/api/personas');    
+    
+    request.success(function(data) {    
+        //console.log(data);
+        var promises = [];
+        // voy a buscar la ultima afiliacion
+        for (var i = 0; i<data.length; i++) {
+            var id = data[i].afiliacionId;
+            var doc = data[i].documento;
+            var nom = data[i].nombre;
+            var ape = data[i].apellido;
+            var promise = $http.get('http://localhost:3000/api/afiliacions/' + id );
+            //console.log(id);
+            promises.push(promise);
+        };
 
-        $scope.data = [];
-        
-        var request = $http.get('/api/personas');    
-        
-        request.success(function(data) {
-            
-            //console.log(data);
-
-            // voy a buscar la ultima afiliacion
+        $q.all(promises).then( function(value){
+            console.log(value);
             for (var i = 0; i<data.length; i++) {
                 var id = data[i].afiliacionId;
                 var doc = data[i].documento;
                 var nom = data[i].nombre;
                 var ape = data[i].apellido;
-                
-                var promise = getAfiliacion.getAfi(id);
-                promise.then (
-                    function(payload) {   
-                        console.log(Afiliacion);
+                var estado = value[i].data.estado;  
+                console.log(i);
+                var todo = {documento:doc, 
+                            nombre:nom, 
+                            apellido:ape, 
+                            estadoafi:estado,
+                            idafi: id};
+                $scope.data.push(todo);
+            }              
+        });    
+    });
+    // Orden de la tabla
+    $scope.sortType     = 'documento'; // set the default sort type
+    $scope.sortReverse  = false;  // set the default sort order
 
-                        var todo = {documento:doc, 
-                                    nombre:nom, 
-                                    apellido:ape, 
-                                    estadoafi:Afiliacion.estado,
-                                    idafi: Afiliacion.id};
-                        $scope.data.push(todo);
-                        console.log(todo);  
-                    }
-                );
-            }
-            console.log($scope.data);
-        });
-        
-        request.error(function(data){
-            console.log('Error: ' + data);
-        });
+    $scope.updPersona = function (cookie) {
+        var now = new Date();
+        var exp = new Date(now);
+        exp.setMinutes(now.getMinutes()+1)
+        $cookies.put('updPersona', cookie, {'expires': exp});
+        window.location.href = "http://localhost:3000/afiliacion";
+    }
 
-        // Orden de la tabla
-        $scope.sortType     = 'documento'; // set the default sort type
-        $scope.sortReverse  = false;  // set the default sort order
+    $scope.setCookie = function (cookie) {
+        var now = new Date();
+        var exp = new Date(now);
+        exp.setMinutes(now.getMinutes()+1)
+        $cookies.put('Pagocookie', cookie, {'expires': exp});
+        window.location.href = "http://localhost:3000/pagos";
+    }
 
-        $scope.updPersona = function (cookie) {
-            var now = new Date();
-            var exp = new Date(now);
-            exp.setMinutes(now.getMinutes()+1)
-            $cookies.put('updPersona', cookie, {'expires': exp});
-            window.location.href = "http://localhost:3000/afiliacion";
-        }
+    $scope.darBaja = function (idafi) {
+        console.log('darBaja');
+        // Hago el upd
+        parameter = JSON.stringify({
+                        estado : 0
+                    });
+        $scope.list = 'http://localhost:3000/api/afiliacions/' + idafi;
+        var request = $http.put($scope.list, parameter);
+        request.success(function (data) {
+            window.location.href = "http://localhost:3000/personas";
+        });        
 
-        $scope.setCookie = function (cookie) {
-            var now = new Date();
-            var exp = new Date(now);
-            exp.setMinutes(now.getMinutes()+1)
-            $cookies.put('Pagocookie', cookie, {'expires': exp});
-            window.location.href = "http://localhost:3000/pagos";
-        }
+    }
+    
+    $scope.darAlta = function (documento) {
+        console.log('darAlta');   
+        // Hago el insert
+        parameter = JSON.stringify({
+                        estado : 1,
+                        documento: documento
+                    });
+        $scope.list = 'http://localhost:3000/api/afiliacions';
+        var request = $http.post($scope.list, parameter);
 
-        $scope.darBaja = function (idafi) {
-            console.log('darBaja');
-            // Hago el upd
+        request.success(function (data) {
+            // Si inserto bien actualizo el dato en la tabla personas
+            $scope.list = 'http://localhost:3000/api/personas/'+ documento;
+
             parameter = JSON.stringify({
-                            estado : 0
-                        });
-            $scope.list = 'http://localhost:3000/api/afiliacions/' + idafi;
+                afiliacionId: data.id
+            });
+
             var request = $http.put($scope.list, parameter);
-            request.success(function (data) {
-                window.location.href = "http://localhost:3000/personas";
-            });        
+            window.location.href = "http://localhost:3000/personas";
+        });        
 
-        }
-        
-        $scope.darAlta = function (documento) {
-            console.log('darAlta');   
-            // Hago el insert
-            parameter = JSON.stringify({
-                            estado : 1,
-                            documento: documento
-                        });
-            $scope.list = 'http://localhost:3000/api/afiliacions';
-            var request = $http.post($scope.list, parameter);
-
-            request.success(function (data) {
-                // Si inserto bien actualizo el dato en la tabla personas
-                $scope.list = 'http://localhost:3000/api/personas/'+ documento;
-
-                parameter = JSON.stringify({
-                    afiliacionId: data.id
-                });
-
-                var request = $http.put($scope.list, parameter);
-                window.location.href = "http://localhost:3000/personas";
-            });        
-
-        }
-    })
+    }
+});
 
