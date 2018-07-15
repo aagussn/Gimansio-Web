328ff79071ea6dcdcd4a4db170b097354b5a72a3
nicolascabrera909 <nicolascabrera909@hotmail.com>
2018-07-14 21:15:53 -0300

    va

--- public/js/asistencia.js
+++ public/js/asistencia.js
@@ -1,80 +1,98 @@
-var app = angular.module('Asistencia', ['ngCookies']);
+var app = angular.module('asistencia', ['ngCookies']);
 
-app.controller('AsistenciaControl', function($scope, $http, $cookies) {
+app.controller('myController', function($scope, $http, $cookies) {
     $scope.data = [];
     
-    var requestPersona = $http.get('/api/personas');  
-    var requestAsistencia = $http.get('/api/asistencia');
+    var requestPersona = $http.get('http://localhost:3000/api/personas');  
+    var requestAsistencia = $http.get('http://localhost:3000/api/asistencia');
+    var requestAfiliacion = $http.get('http://localhost:3000/api/afiliacions');
+
     var auxPersona=null;
     var auxAsistencia=null;
     var auxAfiliacion=null;
     var listaResultado=null;
 
     //Lista de personas
+    function personasList(){
     requestPersona.success(function(data) {
         auxPersona=data;            //Ver de crear un array con los datos de personas y asistencias(un join de ambas)
-        console.log('Data personas.js');
-    });
+        console.log('Data personas.js' + data.length);
+        
+    });}
     requestPersona.error(function(data){
         console.log('Error: ' + data); 
     });
 
     //Lista de asistencias
+      function asistenciasList(){
      requestAsistencia.success(function(data2) {
         auxAsistencia = data2;
-        console.log('Data asistencia.js');
+        console.log('Data asistencia.js ' + data2.length);
+        
     });
     requestAsistencia.error(function(data2){
         console.log('Error: ' + data2);
-    });
+    });}
 
     //Busco lista de afiliaciones
+     function afiliacionList(){
     requestAfiliacion.success(function(data3) {
-        auxPersona=data3;            //Ver de crear un array con los datos de personas y asistencias(un join de ambas)
-        console.log('Data personas.js');
-    });
+        auxAfiliacion=data3;            //Ver de crear un array con los datos de personas y asistencias(un join de ambas)
+        console.log('Data afiliaciones.js' + data3.length);
+       
+    });}
     requestAfiliacion.error(function(data3){
         console.log('Error: ' + data3); 
     });
 
+     console.log("las afis "+auxAfiliacion.length+"las personas "+auxPersona.length+"las asisten "+auxAsistencia.length); 
+   
 
-
-// Creo un array con los datos que nesecito para mostrar
-
+   
   
     //recorro la lista y armo una lista resultante.campos a tener en 
     //cuenta createdAt,updatedAt, id y documento
     //eligo una persona    
-    for(a=0;a<auxPersona.length;a++){
-        laPersona=auxPersona[a];
-        //busco si la persona tiene una afiliacion activa(estado=1)
-        for(b=0;b<auxAfiliacion.length;b++){
-            laAfiliacion=auxAfiliacion[b];
-            if(laAfiliacion.documento==laPersona.documento && laAfiliacion.estado==1){
-                //busco las asistencias del afiliado activo
-                for(c=0;c<auxAfiliacion.length;c++){
-                    laAsistencia=auxAfiliacion[c];
-                    if(laAsistencia.updatedAt>=laAfiliacion.updatedAt){
-                        var paraMostrar = JSON.stringify({
-                            documento: laPersona.documento,
-                            nombre: laPersona.nombre,
-                            apellido:laPersona.apellido,    
-                            fecha: laAsistencia.createdAt,
-                       });
-                        listaResultado.push();
-                        $scope.listaAsistencia=listaResultado;
-                        //Cabiar el valor de la variable $scrope. que quiero luego de los segundos que quiera
-                        setTimeuot(function(){
-                            $scope.$apply(function(){
+    function listadoResult(){
+        for(var a=0;a<auxPersona.length;a++){
+            laPersona=auxPersona[a];
+            //busco si la persona tiene una afiliacion activa(estado=1)
+            for( var b=0;b<auxAfiliacion.length;b++){
+                laAfiliacion=auxAfiliacion[b];
+                if(laAfiliacion.documento==laPersona.documento && laAfiliacion.estado==1){
+                    //busco las asistencias del afiliado activo
+                    for(var c=0;c<auxAfiliacion.length;c++){
+                        laAsistencia=auxAfiliacion[c];
+                        if(laAsistencia.documento==laPersona.documento){
+                            if(laAsistencia.updatedAt>=laAfiliacion.updatedAt){
+                                var paraMostrar = JSON.stringify({
+                                    documento: laPersona.documento,
+                                    nombre: laPersona.nombre,
+                                    apellido:laPersona.apellido,    
+                                    fecha: laAsistencia.createdAt,
+                            });
+                                listaResultado.push();
                                 $scope.listaAsistencia=listaResultado;
-                             });
-                        },2000);
+                                //Cabiar el valor de la variable $scrope. que quiero luego de los segundos que quiera
+                                setTimeuot(function(){
+                                 $scope.$apply(function(){
+                                    $scope.listaAsistencia=listaResultado;
+                                    });
+                                },2000);
+                            }           
+                        }
                     }
                 }
             }
         }
-    }
+    }    
 
+    //PROMESAS
+     personasList().then(function(resultado){return afiliacionList(resultado);})
+                .then(function(nuevoResultado){return asistenciasList(nuevoResultado);})
+                    .then(function(nuevoResultado2){return listadoResult(nuevoResultado2);})
+                        .then(function(resultadoFinal){console.log('Obtenido el resultado final: ' + resultadoFinal);})
+                            .catch(error);
 
     // Orden de la tabla
     $scope.sortType     = 'documento'; // set the default sort type
