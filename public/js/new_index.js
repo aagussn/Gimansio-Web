7f2a4ee006d40751dafbf042521b529d9e6d3067
Agustin Nebril <aguneb@hotmail.com>
2018-10-08 22:07:57 -0300

    cambios en las categorias
    categorias prontas

--- public/js/new_index.js
+++ public/js/new_index.js
@@ -44,12 +44,12 @@ app.controller('Main', function($scope,$http, $mdToast, $q, $window, $rootScope)
 	// Cargo los datos de una persona en la otra pantalla
 	function getUnaPersona(persona){
         // Inicializo los datos en vacio para despues cargarle algo (si tienen) en los combos de Interees
-        $scope.objetivos = {types:[],};
-        $scope.hacerlo = {types:[],};
-        $scope.lograrlo = {types:[],};
-        $scope.interesa = {types:[],};
-        $scope.entere = {types:[],};
-        $scope.avisan = {types:[],};
+        $scope.objetivos  = [];
+        $scope.hacerlos   = [];
+        $scope.lograrlos  = [];
+        $scope.interesas  = [];
+        $scope.enteres    = [];
+        $scope.avisans    = [];
 
         $scope.unaPersona = 0;        
 		
@@ -58,33 +58,33 @@ app.controller('Main', function($scope,$http, $mdToast, $q, $window, $rootScope)
         
         documento = $scope.persona.documento;
 
-        // Voy a buscar los intereses/categorias cargados de la persona
-        var request = $http.get('/api/listCategorias?documento='+ documento).then(function(perCategoria) { 
-            var categorias = perCategoria.data[0].categoria;
+        // Voy a buscar los intereses/categorias cargados en la bd
+        var request = $http.get('/api/itemcategoria').then(function(itemsCategoria) { 
+            var categorias = itemsCategoria.data;
+            console.log(itemsCategoria.data);
 
             for (var i = 0 ; i < categorias.length; i++) {
                 
-                console.log(categorias[i].itemcategorium.id);
-                var  insert = {name: categorias[i].itemcategorium.descripcion, value: categorias[i].itemcategorium.id };
+                var  insert = {name: categorias[i].descripcion, value: categorias[i].id };
 
-                switch(categorias[i].itemcategorium.tipo) {
+                switch(categorias[i].tipo) {
                     case 1:
-                        $scope.objetivos.types.push(insert);
+                        $scope.objetivos.push(insert);
                         break;
                     case 2:
-                        $scope.hacerlo.types.push(insert);
+                        $scope.hacerlos.push(insert);
                         break;
                     case 3:
-                        $scope.lograrlo.types.push(insert);
+                        $scope.lograrlos.push(insert);
                         break;
                     case 4:
-                        $scope.interesa.types.push(insert);
+                        $scope.interesas.push(insert);
                         break;
                     case 5:
-                        $scope.entere.types.push(insert);
+                        $scope.enteres.push(insert);
                         break;                                                
                     case 6:
-                        $scope.avisan.types.push(insert);
+                        $scope.avisans.push(insert);
                         break;                        
                 }
             }
@@ -93,142 +93,55 @@ app.controller('Main', function($scope,$http, $mdToast, $q, $window, $rootScope)
 
         $q.all([request]);
 
+        var selectedObjetivos = [];
+        var selectedHacerlo   = [];
+        var selectedLograrlo  = [];
+        var selectedInteresa  = [];
+        var selectedEntere    = [];
+        var selectedAvisan    = [];        
+
+        // Voy a buscar los datos cargados de la persona
+        var request = $http.get('/api/listPerCategorias?documento='+ documento).then(function(perCategoria) {
+            var categorias = perCategoria.data[0].categoria;
+
+            for (var i = 0 ; i < categorias.length; i++) {
+
+                var insert = categorias[i].itemcategorium.id ;
+
+                switch(categorias[i].itemcategorium.tipo) {
+                    case 1:
+                        selectedObjetivos.push(insert);
+                        break;
+                    case 2:
+                        selectedHacerlo.push(insert);
+                        break;
+                    case 3:
+                        selectedLograrlo.push(insert);
+                        break;
+                    case 4:
+                        selectedInteresa.push(insert);
+                        break;
+                    case 5:
+                        selectedEntere.push(insert);
+                        break;                                                
+                    case 6:
+                        selectedAvisan.push(insert);
+                        break;                        
+                }
+            }
+        });
+
+        $q.all([request]);
+
+        $scope.selectedObjetivos = selectedObjetivos;
+        $scope.selectedHacerlo   = selectedHacerlo;
+        $scope.selectedLograrlo  = selectedLograrlo;
+        $scope.selectedInteresa  = selectedInteresa;
+        $scope.selectedEntere    = selectedEntere;
+        $scope.selectedAvisan    = selectedAvisan;
+
         $scope.unaPersona = 1;
 	};
-
-});
-
-
-// <!-- Objetivos -->
-app.controller('objetivos', function($scope,$http,$q) {
-    var vom = this;
-    vom.selectedOption = '';
-    vom.searchText = '';
-    vom.launchAPIQueryParams = $scope.objetivos;
-    var launchTypeOptions = [];
-
-    var request = $http.get('/api/itemcategoria?tipo=1').then(function(items) { 
-        var item_insert = {};
-        for(i in items.data) {
-            item_insert = {name: items.data[i].descripcion, value: items.data[i].id};
-            launchTypeOptions.push(item_insert);
-        }
-
-    })
-
-    vom.launchTypeOptions = launchTypeOptions; 
-
-    $scope.objetivos = vom.launchAPIQueryParams;
-});
-
-// <!-- Voy a hacerlo -->
-app.controller('hacerlo', function($scope,$http) {
-    var vm = this;
-    vm.selectedOption = '';
-    vm.searchText = '';
-    vm.launchAPIQueryParams = $scope.hacerlo;
-
-    var launchTypeOptions = [];
-
-    var request = $http.get('/api/itemcategoria?tipo=2').then(function(items) { 
-    	var item_insert = {};
-    	for(i in items.data) {
-    		item_insert = {name: items.data[i].descripcion, value: items.data[i].id};
-    		launchTypeOptions.push(item_insert);
-    	}
-
-    });
-
-    vm.launchTypeOptions = launchTypeOptions;
-
-});
-
-// <!-- Voy a lograrlo en -->
-app.controller('lograrlo', function($scope,$http) {
-    var vm = this;
-    vm.selectedOption = '';
-    vm.searchText = '';
-    vm.launchAPIQueryParams = $scope.lograrlo;
-
-    var launchTypeOptions = [];
-
-    var request = $http.get('/api/itemcategoria?tipo=3').then(function(items) { 
-    	var item_insert = {};
-    	for(i in items.data) {
-    		item_insert = {name: items.data[i].descripcion, value: items.data[i].id};
-    		launchTypeOptions.push(item_insert);
-    	}
-
-    });
-
-    vm.launchTypeOptions = launchTypeOptions;
-
-});
-
-// <!-- Me interesa -->
-app.controller('interesa', function($scope,$http) {
-    var vm = this;
-    vm.selectedOption = '';
-    vm.searchText = '';
-    vm.launchAPIQueryParams = $scope.interesa;
-
-    var launchTypeOptions = [];
-
-    var request = $http.get('/api/itemcategoria?tipo=4').then(function(items) { 
-    	var item_insert = {};
-    	for(i in items.data) {
-    		item_insert = {name: items.data[i].descripcion, value: items.data[i].id};
-    		launchTypeOptions.push(item_insert);
-    	}
-
-    });
-
-    vm.launchTypeOptions = launchTypeOptions;
-
-});
-
-// <!-- Me entere por -->
-app.controller('entere', function($scope,$http) {
-    var vm = this;
-    vm.selectedOption = '';
-    vm.searchText = '';
-    vm.launchAPIQueryParams = $scope.entere;
-
-    var launchTypeOptions = [];
-
-    var request = $http.get('/api/itemcategoria?tipo=5').then(function(items) { 
-    	var item_insert = {};
-    	for(i in items.data) {
-    		item_insert = {name: items.data[i].descripcion, value: items.data[i].id};
-    		launchTypeOptions.push(item_insert);
-    	}
-
-    });
-
-    vm.launchTypeOptions = launchTypeOptions;
-
-});
-
-// <!-- Me avisan por -->
-app.controller('avisan', function($scope,$http) {
-
-    var vm = this;
-    vm.selectedOption = '';
-    vm.searchText = '';
-    vm.launchAPIQueryParams = $scope.avisan;
-
-    var launchTypeOptions = [];
-
-    var request = $http.get('/api/itemcategoria?tipo=6').then(function(items) { 
-    	var item_insert = {};
-    	for(i in items.data) {
-    		item_insert = {name: items.data[i].descripcion, value: items.data[i].id};
-    		launchTypeOptions.push(item_insert);
-    	}
-
-    });
-
-    vm.launchTypeOptions = launchTypeOptions;
 
 });
 
