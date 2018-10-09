8132f4773649852bbd1c0f6d13a64440ca777715
nicolascabrera909 <nicolascabrera909@hotmail.com>
2018-10-09 19:08:43 -0300

    con detalles

--- public/js/ingreso.js
+++ public/js/ingreso.js
@@ -11,11 +11,12 @@ app.controller('myController', function($scope,$http,$timeout){
 	}*/
 	
 	//variables globales
-	var tiempo=3000;
+	var tiempo=4000;
 	var f = new Date();
 	var fecha= Date.parse( f.getFullYear() +"-" + (f.getMonth() +1)+ "-" + f.getDate());
 	$scope.bandera=0;
 	var bandera=0;
+	$scope.ingreso = [];
 	console.log('valor inicial $scope.bandera: ' + $scope.bandera + " y bandera "+bandera +" "+ $scope.Documento );
 	
 
@@ -39,72 +40,97 @@ app.controller('myController', function($scope,$http,$timeout){
 			        	var persona=data[0];
 			        	console.log("asigne a la persona");
 			        	//lista afiliaciones
-			        	var lstAfiliaciones=persona.afiliacions;
-			        	for(var b=0;b<lstAfiliaciones.length && !encontrePlan ;b++ ){
-			        		var afiliacion=lstAfiliaciones[0];
-			        		//me quedo con los planes de la afiliacion vigente
-			        		if(afiliacion.plans.length>0){
-				        		var lstPlanes=;
-				        		for(var c=0;c<lstPlanes.length && !encontrePlan ;c++ ){
-				        			plan=lstPlanes[0];
-				        			var fechaPlan=Date.parse(plan.fin);
-				        			if(plan.finn>=fecha){
-				        				if(plan.importepago>0 && plan.pagos.length>0){
-				        					if(plan.importepago==plan.importeplan){
-				        						/*if(pla.cuotasson==plan.cuotasvan){
-						        					//armo el elemento a mostrar
-						        					bandera=1;
-						        					var mostar1 = JSON.stringify({
-												            documento: persona.documento,
-												            nombre: persona.nombre+ " "+persona.apellido,
-												            importeplan:plan.importeplan,
-												            importepago:plan.importepago,
-												            fechaFin: $scope.documento,
-												    });
-						        					$scope.ingreso.push(mostar1); 
-						        				}else{
-						        					if(pla.cuotasson!=plan.cuotasvan){
-		           										console.log('fecha del plan' + fechaPlan);
+				        if(persona.afiliacions.length>0){
+				        	var lstAfiliaciones=persona.afiliacions;
+				        	console.log("asigne la lista de afiliaciones");
+				        	for(var b=0;b<lstAfiliaciones.length && !encontrePlan ;b++ ){
+				        		var afiliacion=lstAfiliaciones[0];
+				        		//me quedo con los planes de la afiliacion vigente
+				        		if(afiliacion.plans.length>0){
+					        		var lstPlanes=afiliacion.plans;
+					        		console.log("asigne lista de planes");
+					        		for(var c=0;c<lstPlanes.length && !encontrePlan ;c++ ){
+					        			plan=lstPlanes[0];
+					        			var fechaPlan=Date.parse(plan.fin);
+					        			if(plan.fin>=fecha){
+					        				console.log("plan vigente");
+					        				if(plan.importepago>0){
+					        					console.log(" pago algo del plan");
+					        					if(plan.importepago==plan.importeplan && pla.cuotasson==plan.cuotasvan){
+					        						console.log(" pago total del plan");
+					        						//marco como pago vigente, todo ok
+							        				bandera=1;
+							        			}else{
+							        				if(plan.importepago<plan.importeplan && pla.cuotasson>plan.cuotasvan){
+							        					console.log("no pago total del plan");
+							        				//verificar si es correcto el impago o tiene margen para pagar
+							        					bandera=2;
+							        				
+							        				}else{bandera=7;}// esto es una inconsistencia de datos revisar el codigo
+							        			}
+									       	}else{bandera=5;}//existe el plan pero no tiene pagos	
+								      	}else{bandera=6;}//no tiene plan vigentes		
+							       	
+								      	//aca tengo todos los  datos armo e json para mostarr en pantalla
+								      	if(bandera==1 || bandera==5 || bandera==2){
+								      		
+								      		var mostar1 = JSON.stringify({
+												documento: persona.documento,
+												nombre: persona.nombre+ " "+persona.apellido,
+												importeplan:plan.importeplan,
+												importepago:plan.importepago,
+												fechaFin: plan.fin,
+												});
+								      		$scope.ingreso.push(mostar1);
+								      		$scope.msjPer=persona;
+								      		// msjPer.documento=persona.documento;
+								      		$scope.msjPer.nombre= persona.nombre;
+								      		// msjPer.apellido=persona.apellido;
+								      		$scope.msjPer.importe=plan.importeplan-plan.importepago;
+								      		$scope.msjPer.fechaFin=plan.fin;
 
-
-						        					}else{bandera=7;}// esto es una inconsistencia de datos revisar el codigo
-
-							       				}	*/
-						      			}else{bandera=5;}//no tiene pagos en el plan vigentes		
-					       			}else{bandera=6;}//no tiene planes vigentes
-						       	}		
-					       	}else{ bandera=3;}//no tiene planes
-			        	}
-			        }
-
-
-
-
-
-			        				// inserto la asistencia de la persona 
-									/*if(bandera==1 || bandera==2 || bandera==3 ){
-					        			// Hago el insert
-		        						parameter = JSON.stringify({
-		                        			personaDocumento: persona.documento
-		                    			});
-										var laAsistencia = $http.post('/api/asistencia',parameter);
-					 					// Voy a buscar el estado de la afiliacion
-					 					laAsistencia.success(function(data4) {
-		           							console.log('Inserte la asisencia' + data);
-					 					});
-					 					laAsistencia.error(function(data2){
-					 						console.log('Error no ingrese asistencia');
-					 					});	
-					        		}*/
-			        			
-		        	  			
-			        		 
-			       	}else{ console.log('No esiste la persona o no tiene afiliacion activa'); bandera=4 }//la persona no tiene afiliacion activa
+								      		// inserto la asistencia de la persona 
+											parameter = JSON.stringify({personaDocumento: persona.documento});
+											var laAsistencia = $http.post('/api/asistencia',parameter);
+						 					// ejecuto el insert afiliacion
+						 					laAsistencia.success(function(data3) {
+			           							console.log('Inserte la asisencia' + data3);
+						 					});
+						 					laAsistencia.error(function(data2){
+						 						console.log('Error no ingrese asistencia');
+						 					});	
+						        			
+					        			}else{
+					        				if(bandera==6 || bandera==3 ){
+					        					var mostar1 = JSON.stringify({
+												documento: persona.documento,
+												nombre: persona.nombre+ " "+persona.apellido,
+												importeplan:"no tiene",
+												importepago:"no tiene",
+												fechaFin: "no tiene",
+												});
+								      		$scope.ingreso.push(mostar1);
+								      		$scope.msjPer=persona;
+								      		// msjPer.documento=persona.documento;
+								      		$scope.msjPer.nombre= persona.nombre;
+								      		// msjPer.apellido=persona.apellido;
+								      		// msjPer.importe=plan.importeplan-plan.importepago;
+								      		// msjPer.fechaFin=plan.fin;
+					        				}
+					        			}	
+							       	}//aca termino el ultimo for
+								}else{bandera=3;}//no tiene planes
+							}
+			   			}else{ bandera=4;}//no tiene afiliacion vigente   
+			   			        		 
+			       	}
 			        	
 			        	
-			    	switch (bandera) { 
-		   		
-								   		case 1: 
+			   
+				}else{bandera=4;}//DATO NO estado VACIO
+					switch (bandera) { 
+	   								
+								   		case 1: //aca el afiliado esta al dia 
 								   			$scope.bandera=bandera;
 											//$scope.antes=$scope.bandera;
 											$timeout(function callAtTimeout() {
@@ -113,7 +139,7 @@ app.controller('myController', function($scope,$http,$timeout){
 											},tiempo);		
 								      		break 
 
-								   		case 2: 
+								   		case 2: //aca el afiliado esta atrasado
 											$scope.bandera=bandera;
 											$timeout(function callAtTimeout() {
 											$scope.bandera=0;
@@ -121,7 +147,7 @@ app.controller('myController', function($scope,$http,$timeout){
 											},tiempo);	      	
 											break 
 
-								   		case 3: //el documento no tiene plan creado
+								   		case 3: // no tiene plan creado 
 								   			$scope.bandera=bandera;
 											//$scope.antes=$scope.bandera;
 											$timeout(function callAtTimeout() {
@@ -130,7 +156,7 @@ app.controller('myController', function($scope,$http,$timeout){
 											},tiempo);	
 								      		break 
 
-										case 4: //el documento no existe o la persona no tiene afiliacion vigente
+										case 4: // no existe la persona o no tiene afiliacion vigente
 											$scope.bandera=bandera;
 											$timeout(function callAtTimeout() {
 											$scope.bandera=0;
@@ -138,7 +164,7 @@ app.controller('myController', function($scope,$http,$timeout){
 											},tiempo);	      	
 											break 
 
-										case 5: //el documento no realizo el pago del plan vigente
+										case 5: //no realizo el pago en el plan vigente
 											$scope.bandera=bandera;
 											$timeout(function callAtTimeout() {
 											$scope.bandera=0;
@@ -146,7 +172,7 @@ app.controller('myController', function($scope,$http,$timeout){
 											},tiempo);	      	
 											break 	
 
-										case 6: //el documento no tiene plan vigente
+										case 6: //no tiene plan vigente
 											$scope.bandera=bandera;
 											$timeout(function callAtTimeout() {
 											$scope.bandera=0;
@@ -165,7 +191,8 @@ app.controller('myController', function($scope,$http,$timeout){
 								   		default: 
 											$scope.bandera=bandera;
 								      		console.log('la bandera no tiene valor ' + bandera); 
-				}
+					}
+
 			});	lstPersonas.error(function(data){ console.log('Error no encontre persona: ' + data);});
 		}//el documento no es null
 	}
